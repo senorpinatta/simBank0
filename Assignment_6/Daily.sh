0b44039815c97d33eacd993cbbe08ba58fc2c72f
BenjaminNMitchell <Benjamin.neil.mitchell@gmail.com>
2017-01-30 21:24:56 -0500

    added front end blackbox testing suite

--- Assignment_6/Daily.sh
+++ Assignment_6/Daily.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+# Get the input variables 
 WEEK_DIR=$1
 DAY_NUM=$2
 VAF_NAME=$3
 MAF_NAME=$4
+# create directory variables
 DAY_DIR=${WEEK_DIR}/DAY_${DAY_NUM}
 TSF_DIR=${DAY_DIR}/TSFs
-MERGED_TSF=${DAY_DIR}/MergedTSF_${DAY_NUM}
+MERGED_TSF=${DAY_DIR}/MergedTSF_${DAY_NUM}.txt
+# Make the new directories 
 mkdir $DAY_DIR
 mkdir $TSF_DIR
 
@@ -15,13 +18,20 @@ while (( $FRONT_END_NUM <= 3 ))
 do
     (( TRANS_NUM = $FRONT_END_NUM + 3 * (DAY_NUM - 1) ))
     TRANS_FILE=Text_Files/Transactions/Transactions${TRANS_NUM}.txt
-    java -classpath FrontEnd FrontEndStartPoint $VAF_NAME TSF.txt < $TRANS_FILE >> Text_Files/ErrorLog.txt
+    java -classpath FrontEnd FrontEndStartPoint $VAF_NAME TSF.txt < $TRANS_FILE >> Text_Files/OutputLog.txt
     cp TSF.txt $TSF_DIR/TSF_${FRONT_END_NUM}.txt
     cat $TSF_DIR/TSF_${FRONT_END_NUM}.txt >> $MERGED_TSF
     ((FRONT_END_NUM++))
 done
 
-java -classpath BackEnd BackEndStartPoint $MAF_NAME $VAF_NAME $MERGED_TSF >> Text_Files/ErrorLog.txt
+# Format the merged Transaction Summary File removing all the End of service lines. 
+# and adding a new one to the end.
+sed /ES\t*/d $MERGED_TSF > temp.txt
+echo ES 00000000 00000000 000 \*\*\* >> temp.txt
+mv temp.txt $MERGED_TSF
+
+# Run the back End on the merged TSF and the input MAF and VAF
+java -classpath BackEnd BackEndStartPoint $MAF_NAME $VAF_NAME $MERGED_TSF >> Text_Files/OutputLog.txt
 
 
 echo "Daily script ${DAY_NUM} is done"
--- Assignment_6/Weekly.sh
+++ Assignment_6/Weekly.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+# initialize command line argument
 WEEK_NUM=$1
 WEEK_DIR=Text_Files/Week_${WEEK_NUM}
+# creates directory for the week
 mkdir $WEEK_DIR
-DAY_NUM=1
 
-echo Starting Week $WEEK_NUM > Text_Files/ErrorLog.txt
+echo Starting Week $WEEK_NUM > Text_Files/OutputLog.txt
 echo Starting Week $WEEK_NUM
 
+#runs the back End and processes and places the files in the required location
+DAY_NUM=1
 while (( DAY_NUM <= 5 ))
 do
-    echo ——Starting Day: ${DAY_NUM}——  >> Text_Files/ErrorLog.txt
+    echo ——Starting Day: ${DAY_NUM}——  >> Text_Files/OutputLog.txt
     # Load in the correct MAF and VAF into the temp files
+    # We use ValidAccounts.txt and MasterAccounts.txt as temporary files
+    # We then move the files to the correct location
     if (( DAY_NUM == 1 ))
       then 
         cp EmptyValidAccounts.txt ValidAccounts.txt
@@ -22,13 +27,14 @@ do
     fi
     # Run the Daily Script 
     bash Daily.sh $WEEK_DIR $DAY_NUM ValidAccounts.txt MasterAccounts.txt
+    # Save the files in the Week’s directory
     cp new_MasterAccounts.txt $WEEK_DIR/MAF_Day_${DAY_NUM}.txt
     cp new_ValidAccounts.txt $WEEK_DIR/VAF_Day_${DAY_NUM}.txt
     ((DAY_NUM++))
     
 done
 
-# clean up by removing temp text files
+# clean up by removing the temporary text files
 rm *new*
 rm MasterAccounts.txt
 rm TSF.txt
